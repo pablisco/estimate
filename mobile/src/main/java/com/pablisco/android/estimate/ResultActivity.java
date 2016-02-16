7e9aaca8db481b6f24976ec2940085c6dd47a087
Pablo Alonso <pabs87@gmail.com>
2016-02-16 00:47:25 +0000

    #4 Made results page actionable

--- mobile/src/main/java/com/pablisco/android/estimate/ResultActivity.java
+++ mobile/src/main/java/com/pablisco/android/estimate/ResultActivity.java
@@ -3,6 +3,7 @@ package com.pablisco.android.estimate;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.pablisco.android.estimate.databinding.ResultActivityBinding;
 
@@ -10,12 +11,13 @@ public class ResultActivity extends AppCompatActivity {
 
     private static final String TAG = ResultActivity.class.getSimpleName();
     public static final String KEY_ESTIMATION = TAG + "#KEY_ESTIMATION";
+    private ResultActivityBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ResultActivityBinding binding;
         binding = DataBindingUtil.setContentView(this, R.layout.result_activity);
+        binding.setValueVisible(false);
         CharSequence estimation = getIntent().getCharSequenceExtra(KEY_ESTIMATION);
         if (estimation != null) {
             binding.setEstimation(estimation);
@@ -24,5 +26,11 @@ public class ResultActivity extends AppCompatActivity {
         }
     }
 
-
+    public void reveal(View view) {
+        if (!binding.getValueVisible()) {
+            binding.setValueVisible(true);
+        } else {
+            finish();
+        }
+    }
 }
