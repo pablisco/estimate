91b8cc2c71ab232f8807d0ea71af12c6c0b2b855
Pablo Alonso <pabs87@gmail.com>
2016-05-25 00:33:36 +0100

    Initial animation

--- mobile/src/main/java/com/pablisco/android/estimate/CardsAdapter.java
+++ mobile/src/main/java/com/pablisco/android/estimate/CardsAdapter.java
@@ -1,7 +1,11 @@
 package com.pablisco.android.estimate;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -43,15 +47,30 @@ class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.ViewHolder> {
 
         @SuppressWarnings({"UnusedParameters", "unused"})
         void onCardSelected(View view) {
-            Context context = view.getContext();
-            Intent intent = new Intent(context, ResultActivity.class);
+            Activity activity = findActivityFor(view);
+            Intent intent = new Intent(activity, ResultActivity.class);
             intent.putExtra(ResultActivity.EXTRA_ESTIMATION, cardText);
-            context.startActivity(intent);
+            activity.startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(activity, null).toBundle());
         }
 
         void setCardText(CharSequence cardText) {
             this.cardText = cardText;
             ((TextView)itemView).setText(cardText);
+        }
+    }
+
+    @Nullable
+    private static Activity findActivityFor(View view) {
+        Context context = view.getContext();
+        if (context instanceof ContextWrapper) {
+            while(!(context instanceof Activity)) {
+                context = ((ContextWrapper) context).getBaseContext();
+            }
+        }
+        if (context instanceof Activity) {
+            return (Activity) context;
+        } else {
+            throw new IllegalStateException("No activity found for " + view);
         }
     }
 }
